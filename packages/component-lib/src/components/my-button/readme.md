debf4c31954e5d52b1b0c897dc87bf8904d9e332
Lody Borgers <l.borgers@taf.nl>
2023-02-14 15:19:01 +0100

    Built React version of component lib

---
+++ packages/component-lib/src/components/my-button/readme.md
@@ -0,0 +1,17 @@
+# my-button
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property  | Attribute | Description | Type      | Default     |
+| --------- | --------- | ----------- | --------- | ----------- |
+| `primary` | `primary` |             | `boolean` | `undefined` |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
--- packages/component-lib/stencil.config.ts
+++ packages/component-lib/stencil.config.ts
@@ -1,8 +1,14 @@
 import { Config } from '@stencil/core';
+import { reactOutputTarget as react } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'component-lib',
   outputTargets: [
+    react({
+      componentCorePackage: 'component-lib-react',
+      proxiesFile: '../component-lib-react/src/components/stencil-generated/index.ts',
+      includeDefineCustomElements: true,
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
